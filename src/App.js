1fe95246cd0af0ee4f59297b78caf0623513ac6c
David L <davidjlee@gmail.com>
2019-01-15 11:19:36 -0500

    src/App.js updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
  import Landing from './components/Landing';
  import Library from './components/Library';
 
+ import Album from './components/Album';
+
 class App extends Component {
   render() {
     return (
@@ -12,14 +14,15 @@ class App extends Component {
         <header>
           <nav>
              <Link to='/'>Landing</Link>
-             
-             <Link to='/library'>Library</Link>
+             <Link to='/Library'>Library</Link>
+              
            </nav>
           <h1>Bloc Jams</h1>
           </header>
           <main>
                 <Route exact path="/" component={Landing} />
               <Route path="/library" component={Library} />
+              <Route path="/album" component={Album} />
           </main>
       </div>
     );
